2b030df48ea445f66b6ceb1e9a37a88259d289ec
LinneaWestholm <linnea.westholm@hotmail.com>
2025-10-03 09:33:46 +0200

    la till scrapers för aftonbladet och svt

---
+++ Scrapers/AftonbladetScraper.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import os
+import time
+
+def scrape_aftonbladet(max_articles=20):
+    url = "https://www.aftonbladet.se/minekonomi/"
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+    )
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    time.sleep(5)
+
+    html = driver.page_source
+    os.makedirs("Debug", exist_ok= True)
+    with open("Debug/aftonbladet_debug.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
+    soup = BeautifulSoup(html, "html.parser")
+    articles = []
+
+    headline_tags = soup.find_all("div", class_="hyperion-css-1ooqwy6")
+    count = 0
+
+    for tag in headline_tags:
+        if count >= max_articles:
+            break
+
+        link_tag = tag.find("a", attrs={"data-test-tag":"internal-link"})
+        link = link_tag.get("href") if link_tag else "No link"
+        if link and not link.startswith("/minekonomi/"):
+            link = "https://www.aftonbladet.se/minekonomi/" + link
+
+        headline_tag = tag.find("h2")
+        headline = headline_tag.get_text(strip=True) if headline_tag else "No headline"
+
+        summary_tag = tag.find("p")
+        summary = summary_tag.get_text(strip=True) if summary_tag else "No summary"
+
+        articles.append({
+            "Headline": headline,
+            "Link": link,
+            "Summary": summary
+        })
+
+    driver.quit()
+    df_new = pd.DataFrame(articles)
+    os.makedirs("Articles",exist_ok=True )
+    csv_path = "Articles/aftonbladet_articles.csv"
+
+    count += 1
+
+    if os.path.exists(csv_path):
+        df_existing = pd.read_csv(csv_path)
+        df_combined = pd.concat([df_existing, df_new], ignore_index=True)
+        df_combined = df_combined.drop_duplicates(subset="Link", keep="first")
+    else:
+         df_combined = df_new
+         
+    df_combined.to_csv(csv_path, index=False, encoding="utf-8")
+    print(f"Saved {csv_path} with {len(df_combined)} total articles")
+
+if __name__ == "__main__":
+    scrape_aftonbladet(max_articles=20)
+
+       
+
+
+
+
+         
+
