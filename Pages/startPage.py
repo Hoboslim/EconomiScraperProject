3fa96989e6f3226fcdd6587468d83114a2596572
LinneaWestholm <linnea.westholm@hotmail.com>
2025-10-07 10:27:13 +0200

    fixed startpage

--- Pages/startPage.py
+++ Pages/startPage.py
@@ -1,17 +1,17 @@
-import tkinter as tk
+import customtkinter as ctk
 
 
 
-class StartPage(tk.Frame):
+class StartPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         
         
-        tk.Label(self, text="Welcome to Webscraper!", font=("Arial", 18, "bold")).pack(pady=20)
-        tk.Label(self, text="This app lets you scrape articles and classify them using AI.", font=("Arial", 14, "bold")).pack(pady=10)
+        ctk.CTkLabel(self, text="Welcome to Webscraper!", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=20)
+        ctk.CTkLabel(self, text="This app lets you scrape articles and classify them using AI.", font=ctk.CTkFont(size=14, weight="bold")).pack(pady=10)
         
-        tk.Button(self, text="Go to Scraper Page", command=lambda: controller.show_frame("ScraperPage")).pack(pady=10)
-        tk.Button(self, text="Go to Classification Page", command=lambda: controller.show_frame("ClassificationPage")).pack(pady=10)
-        tk.Button(self, text="Go to Result Page", command=lambda: controller.show_frame("ResultPage")).pack(pady=10)
+        ctk.CTkButton(self, text="Go to Scraper Page", command=lambda: controller.show_frame("ScraperPage")).pack(pady=10)
+        ctk.CTkButton(self, text="Go to Classification Page", command=lambda: controller.show_frame("ClassificationPage")).pack(pady=10)
+        ctk.CTkButton(self, text="Go to Result Page", command=lambda: controller.show_frame("ResultPage")).pack(pady=10)
         
         
