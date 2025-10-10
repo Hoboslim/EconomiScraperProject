09eb749c6c11993a0e72b54fc327e0769ac4e882
LinneaWestholm <linnea.westholm@hotmail.com>
2025-10-10 12:52:30 +0200

    Added textboxes to readmepage

--- Pages/readMePage.py
+++ Pages/readMePage.py
@@ -3,12 +3,41 @@ import customtkinter as ctk
 class ReadMePage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        ctk.CTkLabel(self, text="Instructions", font=ctk.CTkFont(size=30, weight="bold")).pack(pady=10)
-        ctk.CTkLabel(self, text="How to use Webscraper:", font=ctk.CTkFont(size=15, weight="bold")).pack(pady=10)
-        
-        ctk.CTkButton(self, text="Read ME:", command=self.show_readme).pack(pady=10)
+
+
+        ctk.CTkLabel(self, text="Read Me Page", font=ctk.CTkFont(size=20, weight="bold")).pack(pady=10)
+        ctk.CTkButton(self, text="Read Instructions", command=self.show_instructions).pack(pady=10)
+        self.textbox1 = ctk.CTkTextbox(self, width=600, height=300)
+
+        ctk.CTkButton(self, text="Read ME", command=self.show_readme).pack(pady=10)
+        self.textbox2 = ctk.CTkTextbox(self, width=600, height=200)
         ctk.CTkButton(self, text="Back to Home", command=lambda: controller.show_frame("StartPage")).pack(side="bottom", pady=10)
+
+    def show_instructions(self):
+        self.textbox1.pack(pady=10)
         
+        with open("texts/Instructions.txt", "r", encoding="utf-8") as f:
+            instructions_text = f.read()
+            
+        self.textbox1.insert("0.0", instructions_text)
+        self.textbox1.configure(state="disabled")
+
+     
+    def show_readme(self):
+        self.textbox2.pack(pady=10)
+        
+        with open("texts/README.txt", "r", encoding="utf-8") as f:
+            readme_text = f.read()
+            
+        self.textbox2.insert("0.0", readme_text)
+        self.textbox2.configure(state="disabled")
+      
+    
+
+    
+            
+
+
         
        
 
---
+++ texts/README.txt
@@ -0,0 +1,9 @@
+Webscraper
+
+Webscraper is an desktop app built using python and the program Ollama. 
+It offers a combination of backend using python and frontend using the tkinter-module
+and customtkinter library.
+
+It's an app for anyone intrested in knowing the current state of the economy 
+both internationally and locally. Webscraper scrape news articles from news websites
+with the topic economy/busniess specifically. 
